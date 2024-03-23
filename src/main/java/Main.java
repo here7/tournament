82c9ea6f327a3808569bd867375322f645ae1a34
dani heredia <daniel.heredia@manomano.com>
2024-03-23 13:15:40 +0100

    All tests passed. LinkedHashMap sorted by Score + TeamName

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Tournament tournament = new Tournament();
-
         tournament.applyResults("Blithering Badgers;Allegoric Alaskans;loss");
 
         tournament.printTable();
